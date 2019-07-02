ece702a6ddaaa36d9bf08e6f363855ffe1f1636b
Lasse Laukkanen <lasse.laukkanen@bitwise.fi>
2019-07-02 16:21:15 +0300

    Setup webpack and babel

--- frontend/cooplist/src/components/App.js
+++ frontend/cooplist/src/components/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>Page One </div>;
+}
+
+const PageTWo = () => {
+    return <div> Page Two </div>;
+}
+
 
 class App extends React.Component {
     constructor(props) {
@@ -6,7 +16,20 @@ class App extends React.Component {
     }
 
     render() {
-        return( <div> App Comnponent </div> );
+        return(
+
+            <div>
+                <h1> Hello there </h1>
+                <div>
+                    <BrowserRouter>
+                        <div>
+                            <Route path="/" exact component={PageOne}/>
+                            <Route path="/login" exact component={PageTWo}/>
+                        </div>
+                    </BrowserRouter>
+                </div>
+            </div>
+        );
     }
 
 
--- frontend/cooplist/src/index.js
+++ frontend/cooplist/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+
+
 
 
 ReactDOM.render(
